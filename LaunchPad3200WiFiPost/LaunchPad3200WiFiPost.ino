0708438d639b2e682d63c36a5aa70a4660d49bc5
crabmeat99 <harper@cs.ucf.edu>
2019-05-04 13:36:33 -0400

    updated with ENERGIA_PLATFORM defined for new 2.2.0 M2XStreamClient

--- LaunchPad3200WiFiPost/LaunchPad3200WiFiPost.ino
+++ LaunchPad3200WiFiPost/LaunchPad3200WiFiPost.ino
@@ -1,22 +1,28 @@
+#define ENERGIA_PLATFORM 0
+
 #include <aJSON.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <BMA222.h>
 #include "SPI.h"
+#include "Adafruit_TMP006.h"
 
 #include "M2XStreamClient.h"
 
-char ssid[] = "UCF_GUEST"; //  your network SSID (name)
+char ssid[] = "UCF_DEMO"; //  your network SSID (name)
 char pass[] = "UCF_UCF_IOT";    // your network password (use for WPA, or use as key for WEP)
 //int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
 char deviceId[] = "8d4011b4b7b1acea472e7489b3b163e3"; // Device you want to push to
-char streamName[] = "temperature"; // Stream you want to push to
+char streamName_t[] = "temperature"; // Stream you want to push temperature to
+char streamName_z[] = "acceleration_z"; // Stream you want to push accleration_z to
 char m2xKey[] = "31b158f2c5a9779e6b149bc076a75c48"; // Your M2X access key
 
 BMA222 mySensor;
+
+Adafruit_TMP006 tmp006(0x41);  // start with a diferent i2c address!
 
 WiFiClient client;
 
@@ -26,6 +32,14 @@ void setup() {
 
   Serial.begin(9600);
 
+  // you can also use tmp006.begin(TMP006_CFG_1SAMPLE) or 2SAMPLE/4SAMPLE/8SAMPLE to have
+  // lower precision, higher rate sampling. default is TMP006_CFG_16SAMPLE which takes
+  // 4 seconds per reading (16 samples)
+  if (! tmp006.begin()) {
+    Serial.println("No sensor found");
+    while (1);
+  }
+  
   mySensor.begin();
   uint8_t chipID = mySensor.chipID();
   Serial.print("chipID: ");
@@ -63,6 +77,9 @@ void setup() {
 
 void loop() {
 
+  //tmp006.wake();
+  //tmp006.sleep();
+      
   float x = mySensor.readXData()/65.0;
   Serial.print("Accel X: ");
   Serial.print(x);
@@ -75,35 +92,28 @@ void loop() {
   Serial.print(", Z: ");
   Serial.println(z);
 
-  // Calculate pitch and roll. Find the maximum tilt angle.
-  float pitch = atan(x / sqrt(y * y + z * z))*180;
-  float roll = atan(y / sqrt(x * x + z * z))*180;
-  float maxTilt =
-    max(abs(roll), abs(pitch)) / 3.14159;
-  Serial.print("pitch: ");
-  Serial.print(pitch);
-  Serial.print(" roll: ");
-  Serial.print(roll);
-  Serial.print(" maxTilt: ");
-  Serial.println(maxTilt);
+  float objt = tmp006.readObjTempC();
+  Serial.print("Object Temperature: "); Serial.print(objt); Serial.println("*C");
+  float diet = tmp006.readDieTempC();
+  Serial.print("Die Temperature: "); Serial.print(diet); Serial.println("*C");
 
+  int response = m2xClient.updateStreamValue(deviceId, streamName_t, objt);
+  Serial.print("M2x client response code: ");
+  Serial.println(response);
 
-  // If the maximum tilt is over 20 degrees, then send
-  // data to stream
-  if (maxTilt > 20) {
-    int response = m2xClient.updateStreamValue(deviceId, streamName, maxTilt);
-    Serial.print("M2x client response code: ");
-    Serial.println(response);
+  if (response == -1)
+    while (1)
+    ;
 
-    if (response == -1)
-      while (1)
-        ;
+  response = m2xClient.updateStreamValue(deviceId, streamName_z, z);
+  Serial.print("M2x client response code: ");
+  Serial.println(response);
 
-    delay(500);
-  }
+  if (response == -1)
+    while (1)
+    ;
 
-
-
+  delay(5000);
 }
 
 void printWifiStatus() {
